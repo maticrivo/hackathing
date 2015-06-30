92766f36d0629e4fd081109e1517c112370043d6
Ran Ben Aharon <ran@ranbena.com>
2015-06-30 10:40:15 +0300

    Fixed db connection timeout issue

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,23 @@
-import os, logging
+import logging
 from flask import Flask
 from flask_googlelogin import GoogleLogin
-
 from app import settings
+from app.db import db
+from app.jinja import setup_jinja
 
-logger = logging.getLogger(__name__)
+
+def setup_app():
+    app = Flask(__name__)
+    app.config.update(
+        GOOGLE_LOGIN_CLIENT_ID=settings.GOOGLE_LOGIN['CLIENT_ID'],
+        GOOGLE_LOGIN_CLIENT_SECRET=settings.GOOGLE_LOGIN['CLIENT_SECRET'],
+        GOOGLE_LOGIN_REDIRECT_URI=settings.GOOGLE_LOGIN['REDIRECT_URI'],
+        GOOGLE_LOGIN_SCOPES='email',
+        SECRET_KEY='hackathing_secret'
+    )
+    db.init_app(app)
+    return app, GoogleLogin(app=app)
+
 
 def setup_logging():
     handler = logging.StreamHandler()
@@ -14,33 +27,23 @@ def setup_logging():
     logging.getLogger().setLevel(settings.LOG['level'])
     logging.getLogger("passlib").setLevel("ERROR")
 
-setup_logging()
-
-app = Flask(__name__)
-app.config.update(
-    # GOOGLE_LOGIN_CLIENT_ID=os.environ['GOOGLE_LOGIN_CLIENT_ID'],
-    # GOOGLE_LOGIN_CLIENT_SECRET=os.environ['GOOGLE_LOGIN_CLIENT_SECRET'],
-    # GOOGLE_LOGIN_REDIRECT_URI='http://127.0.0.1:5000/oauth2callback',
-    GOOGLE_LOGIN_CLIENT_ID=settings.GOOGLE_LOGIN['CLIENT_ID'],
-    GOOGLE_LOGIN_CLIENT_SECRET=settings.GOOGLE_LOGIN['CLIENT_SECRET'],
-    GOOGLE_LOGIN_REDIRECT_URI=settings.GOOGLE_LOGIN['REDIRECT_URI'],
-    GOOGLE_LOGIN_SCOPES='email',
-    SECRET_KEY='hackathing_secret'
-)
-auth = GoogleLogin(app=app)
-
-
-from hypchat import HypChat
-from settings import HIPCHAT
-hc = HypChat(HIPCHAT['token'], endpoint="https://evme.hipchat.com")
-
-from app.jinja import setup_jinja
-setup_jinja(app)
-
-def send_hipchat_msg(user, msg):
-    if user == 'ran@everything.me':
-        hc.get_user(user).message(msg)
+    return logging.getLogger(__name__)
 
 def email(name):
     return name+'@everything.me'
+
+logger = setup_logging()
+app, auth = setup_app()
+setup_jinja(app)
+
+
+# from hypchat import HypChat
+# from settings import HIPCHAT
+
+# hc = HypChat(HIPCHAT['token'], endpoint="https://evme.hipchat.com")
+
+# def send_hipchat_msg(user, msg):
+#     if user == 'ran@everything.me':
+#         hc.get_user(user).message(msg)
+
 
--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
-from app import app, settings, email, send_hipchat_msg
+from app import app
+# from app import settings, email, send_hipchat_msg
 from flask import render_template, url_for, redirect, request, flash
 from models import *
 from login import requires_login
@@ -161,40 +162,40 @@ def skill(id):
 def agenda():
     return render_template('agenda.html', ctx={})
 
-@app.route('/api/digest')
-def digest():
-    items = []
-
-    # no pitch, no team
-    for user in Hackers.non_active():
-        suggested = Projects.suggest_by_hacker(user)
-        suggested_count = len(list(suggested))
-        params = dict(user=user.user, count=suggested_count)
-
-        if suggested_count == 1:
-            msg_type = 'no_team_1_sugg'
-            params['id'] = suggested.get().id
-        elif suggested_count > 1:
-            msg_type = 'no_team_much_sugg'
-        else:
-            msg_type = 'no_team_0_sugg'
-
-        msg = settings.MESSAGES[msg_type].format(**params)
-        items.append((email(user.user), msg))
-
-    # much pitches
-    for user in Hackers.pitched_over_one():
-        msg = settings.MESSAGES['pitch_much'].format(user=user.user, count=user.count)
-        items.append((email(user.user), msg))
-
-    # much joins
-    for user in Hackers.teamed_over_one():
-        msg = settings.MESSAGES['team_much'].format(user=user.user, count=user.count)
-        items.append((email(user.user), msg))
-
-    for msg in items:
-        send_hipchat_msg(*msg)
-
-    return render_template('digest.html', ctx=items)
+# @app.route('/api/digest')
+# def digest():
+#     items = []
+#
+#     # no pitch, no team
+#     for user in Hackers.non_active():
+#         suggested = Projects.suggest_by_hacker(user)
+#         suggested_count = len(list(suggested))
+#         params = dict(user=user.user, count=suggested_count)
+#
+#         if suggested_count == 1:
+#             msg_type = 'no_team_1_sugg'
+#             params['id'] = suggested.get().id
+#         elif suggested_count > 1:
+#             msg_type = 'no_team_much_sugg'
+#         else:
+#             msg_type = 'no_team_0_sugg'
+#
+#         msg = settings.MESSAGES[msg_type].format(**params)
+#         items.append((email(user.user), msg))
+#
+#     # much pitches
+#     for user in Hackers.pitched_over_one():
+#         msg = settings.MESSAGES['pitch_much'].format(user=user.user, count=user.count)
+#         items.append((email(user.user), msg))
+#
+#     # much joins
+#     for user in Hackers.teamed_over_one():
+#         msg = settings.MESSAGES['team_much'].format(user=user.user, count=user.count)
+#         items.append((email(user.user), msg))
+#
+#     for msg in items:
+#         send_hipchat_msg(*msg)
+#
+#     return render_template('digest.html', ctx=items)
 
 
